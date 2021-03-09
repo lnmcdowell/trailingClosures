ad18cfc021a74eef33035933c1fa4f871e51d145
lnmcdowell <nathaniel29651@gmail.com>
2021-03-09 14:12:58 -0500

    dictionary and array of functions. Also functions accepting a function with arguments as a parameter

--- trailingClosures/main.swift
+++ trailingClosures/main.swift
@@ -7,5 +7,45 @@
 
 import Foundation
 
-print("Hello, World!")
+func newFunction(insideFx: (String) -> String){
+    print("Just going to say")
+    print("The answer is \(insideFx("hello"))")
+    print("Just then!")
+}
+
+func runIt(inStr:String)->String {
+    return inStr + " added"
+}
+
+
+
+var funcArray = [()->()]()
+funcArray.append {
+    print("hello")
+}
+funcArray.append {
+    print(" there")
+}
+funcArray.append {
+    print("again")
+}
+
+newFunction(insideFx: runIt)
+
+for myFun in funcArray{
+    myFun()
+}
+funcArray[1]()
+
+var funcDict = [String: ()->()]()
+funcDict.updateValue({print("first")}, forKey: "first")
+funcDict.updateValue({print("this is the second function")}, forKey: "second")
+funcDict.updateValue({print("this is the extra function")}, forKey: "extra")
+print("\(funcDict.values.count)")
+funcDict.randomElement()?.value() //retrieve random function
+funcDict.removeValue(forKey: "extra")
+print("\(funcDict.values.count)")
+funcDict["second"]!()
+
+
 
